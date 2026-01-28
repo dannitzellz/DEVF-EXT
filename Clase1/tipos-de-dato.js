5a2af76bbcddcb520e370633f317fc58be55e762
dannitzellz <Dannitzel14@gmail.com>
2026-01-27 20:23:56 -0600

    Ejemplos de tipos de datos en JavaScript

---
+++ Clase1/tipos-de-dato.js
@@ -0,0 +1,45 @@
+console.log(42);
+// number
+
+console.log('Veinticinco');
+// string
+
+console.log(-666);
+// number
+
+console.log(true);
+// boolean
+
+console.log(0);
+// number
+
+console.log('');
+// string
+
+console.log(null);
+// object
+
+console.log(undefined);
+// undefined
+
+let FALSE = false;
+console.log(FALSE);
+// boolean
+
+console.log(3.14);
+// number
+
+console.log('');
+// string
+
+console.log(NaN);
+// number
+
+console.log([1, 2, 3]);
+// object
+
+console.log({ nombre: 'Danna' });
+// object
+
+console.log(function () {});
+// function
