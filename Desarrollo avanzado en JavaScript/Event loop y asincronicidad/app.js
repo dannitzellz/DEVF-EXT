d5726f92ab06c62abcf9fd561f5357cf7882a86c
dannitzellz <Dannitzel14@gmail.com>
2026-04-20 21:34:26 -0600

    Agrega ejercicio de simulador de pedidos de una cafeteria implementando asincronicidad

---
+++ Desarrollo avanzado en JavaScript/Event loop y asincronicidad/app.js
@@ -0,0 +1,32 @@
+const orderList = document.getElementById('orderList');
+const addOrderBtn = document.getElementById('addOrderBtn');
+
+let orderId = 1; // Para identificar los pedidos
+
+addOrderBtn.addEventListener('click', () => {
+    const order = { id: orderId++, status: 'En Proceso' };
+    addOrder(order);
+    processOrder(order);
+});
+
+function addOrder(order) {
+    const listItem = document.createElement('li');
+    listItem.id = `order-${order.id}`;
+    listItem.textContent = `Pedido #${order.id}: ${order.status}`;
+    orderList.appendChild(listItem);
+}
+
+function updateOrderStatus(order, status) {
+    const listItem = document.getElementById(`order-${order.id}`);
+    if (listItem) {
+        listItem.textContent = `Pedido #${order.id}: ${status}`;
+        if (status === 'Completado') {
+            listItem.classList.add('completed');
+        }
+    }
+}
+
+async function processOrder(order) {
+    const randomTime = Math.floor(Math.random() * 4000) + 1000;
+    setTimeout(()=> updateOrderStatus(order, 'Completado'), randomTime)
+}
